3be78b5557be79d3d19e2eaba11013bf8875e1b1
Hubaldium <jake.mieth@icloud.com>
2026-01-29 13:50:08 +0100

    Start

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from .database import create_db_and_tables
 from .routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -11,6 +12,17 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     title="Speedrun Leaderboard API",
     lifespan=lifespan
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://theboss-png.github.io",
+        "http://127.0.0.1:5500"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # --- Root-Endpunkt ---
